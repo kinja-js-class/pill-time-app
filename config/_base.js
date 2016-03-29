34df1d2d087a1599e4962c75aa96b8f1a0cfc0b0
Janos Hardi <janos.hardi@gawker.com>
2016-03-29 17:09:17 +0200

    adding firebase

--- config/_base.js
+++ config/_base.js
@@ -78,7 +78,8 @@ config.globals = {
   '__TEST__'     : config.env === 'test',
   '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
   '__DEBUG_NEW_WINDOW__' : !!argv.nw,
-  '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
+  '__BASENAME__' : JSON.stringify(process.env.BASENAME || ''),
+  '__FIREBASE_URL__' : 'https://incandescent-fire-8559.firebaseio.com/patients'
 }
 
 // ------------------------------------
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
+import {firebaseStateReducer as firebase} from 'redux-react-firebase'
 
 export default combineReducers({
+  firebase,
   router
 })
