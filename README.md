113467caf0e157f6ad2c16d658b9ec3a13c36aaf
Janos Hardi <janos.hardi@gawker.com>
2016-03-30 17:35:47 +0200

    back to the future

--- README.md
+++
@@ -1,1 +0,0 @@
-### [Original Starter Kit README](https://github.com/davezuko/react-redux-starter-kit)
--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { routerReducer as router } from 'react-router-redux'
-import {firebaseStateReducer as firebase} from 'redux-react-firebase'
-
-export default combineReducers({
-  firebase,
-  router
-})
